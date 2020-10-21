1222aef097e46aa6ce009c033f14fc7c5bdf8987
DharoOficial <erickbandeiradharo@gmail.com>
2020-10-21 10:31:45 -0300

    projeto criado

---
+++ src/components/index.jsx
@@ -0,0 +1,20 @@
+// import react from 'react';
+
+// function rodape (){
+//     return(
+//         <footer>
+//             <p>diretos reservados</p>
+//         </footer>
+//     )
+// }
+
+
+// // function cabecario (){
+// //     return (
+// //         <header>
+// //             <p>Pagina Login</p>
+// //         </header>
+// //     )
+// // }
+// export default rodape;
+
